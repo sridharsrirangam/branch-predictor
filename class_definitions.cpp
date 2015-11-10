a4bea083277f1ad8fe25bc5be65439cfb074de0c
sridhar <srirangam.sridhar@gmail.com>
2015-11-09 20:47:26 -0500

    bimodal working, need to add misrates

--- class_definitions.cpp
+++ class_definitions.cpp
@@ -8,25 +8,95 @@ class BTB_block
   public:
     bool valid;
     ulong tag;
+    unsigned int count;
     
     BTB_block()
     {
       valid = 0;
       tag = 0;
+      count = 2;
     }
 
-}
+};
 
 class BTB
 {
   public:
     BTB_block * btb_block;
-
-    BTB();
-    BTB(int M1)
+    unsigned int size;
+    BTB(){};
+    void BTB_c(unsigned int M1)
     {
-      btb_block = new btb_block[M1];
+      size = M1;
+      btb_block = new BTB_block[M1];
     }
+  
+    int find_tag_in_BTB(ulong, ulong);
+    void fill_tag_in_BTB(ulong, ulong);
+    unsigned int  get_prediction_count(ulong);
+    void increment_prediction_count(ulong);
+    void decrement_prediction_count(ulong);
+    void print_stats();
+};
 
+int BTB::find_tag_in_BTB (ulong tag, ulong index)
+{
+  int pos;
+  int hit = 0;
 
+    if((tag == btb_block[index].tag) && (btb_block[index].valid == 1))
+    {
+      hit =1;
+      
+    }
+  return hit;  
+}
 
+void BTB::fill_tag_in_BTB(ulong tag, ulong index)
+{
+  btb_block[index].tag = tag;
+  btb_block[index].valid = 1;
+}
+
+unsigned int  BTB::get_prediction_count(ulong index)
+{
+  return btb_block[index].count;
+}
+
+void BTB::increment_prediction_count(ulong index)
+{
+  if(btb_block[index].count<3)
+  {
+    btb_block[index].count++;
+  }
+}
+
+void BTB::decrement_prediction_count(ulong index)
+{
+  if(btb_block[index].count>0)
+  {
+    btb_block[index].count--;
+  }
+}
+
+void BTB::print_stats()
+{
+  std::cout<<"Final Bimodal Contents"<<std::endl;
+  for(int i = 0; i<size;i++)
+  {
+    std::cout<<i<<" "<<get_prediction_count(i)<<std::endl;
+  }
+}
+
+class Bimodal
+{
+  public:
+  BTB pred_table;
+
+  Bimodal(){};
+ void  Bimodal_c(unsigned int M2)
+  {
+    pred_table.BTB_c(M2);
+  }
+
+};
--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <sstream>
 #include <fstream>
 
+#include <math.h>
+
+#include "class_definitions.cpp"
+
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -12,9 +16,14 @@ int main(int argc, char *argv[])
   string trace_file;
   char  addr_str[10];
   unsigned long addr;
+  unsigned long index;
   char actual_br;
-
-
+  unsigned int M2;
+  unsigned long size;
+  char pred_br;  
+  
+  Bimodal bimodal;
+  
   if(argc != 2)
   {
     cout<<"invalid arguments"<<endl;
@@ -26,12 +35,14 @@ int main(int argc, char *argv[])
   
   FILE *pFile;
   char *fname = (char *)malloc(30);
-  fname = argv[2];
+  fname = argv[3];
 
 
   if(strcmp(brpr_model,"bimodal") == 0)
-  cout<<"branch predictor type: "<<brpr_model<<endl;
-  
+  {
+    cout<<"branch predictor type: "<<brpr_model<<endl;
+    M2 = atoi(argv[2]);
+  }
   if(strcmp(brpr_model,"gshare") == 0)
   cout<<"branch predictor type: "<<brpr_model<<endl;
   
@@ -46,13 +57,46 @@ int main(int argc, char *argv[])
     cout<<"trace file problem"<<endl;
     exit(0);
   }
+
+  size = pow(2,M2);
+  bimodal.Bimodal_c(size);
   while(fscanf(pFile,"%s %c",&addr_str, &actual_br))
   {
     if(feof(pFile)) break;
     addr = strtoul(addr_str,NULL,16);
-    cout<<"addr is: "<<addr<<" actual taken: "<<actual_br<<endl;
-  }
+   // cout<<hex<<addr<<" " <<actual_br<<endl;
+    addr = addr/4;
+    index = addr % (unsigned long)(pow(2,M2));
+    if(strcmp(brpr_model,"bimodal") == 0)
+    {
     
-  return 0;
+    // cout<<" BP "<<dec<<index<<" "<<bimodal.pred_table.get_prediction_count(index);
+     int prev_pred = bimodal.pred_table.get_prediction_count(index);
+     if(prev_pred >= 2)
+     {
+       pred_br = 't';
+     }
+     else if(prev_pred <2)
+     {
+       pred_br = 'n';
+     }
+     // cout<<"prediction : "<<pred_br<<endl; 
+      if((pred_br =='t') && (actual_br == 't'))
+          { bimodal.pred_table.increment_prediction_count(index); }
+      
+      if((pred_br =='t') && (actual_br == 'n'))
+          { bimodal.pred_table.decrement_prediction_count(index); }
 
+      if((pred_br =='n') && (actual_br == 't'))
+          { bimodal.pred_table.increment_prediction_count(index); }
+
+      if((pred_br =='n') && (actual_br == 'n'))
+          { bimodal.pred_table.decrement_prediction_count(index); }
+    
+    // cout<<" BU "<<dec<<index<<" "<<bimodal.pred_table.get_prediction_count(index)<<endl;
+     }
+  
+  }
+   bimodal.pred_table.print_stats(); 
+  return 0;
 }
